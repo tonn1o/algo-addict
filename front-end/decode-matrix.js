be88244e47bbc7f09e98b2e4811affa9d658ed8d
Toni Babenko <a.babenko.e@gmail.com>
2022-01-26 00:19:09 +0100

    add decode-matrix.js

---
+++ front-end/decode-matrix.js
@@ -0,0 +1,52 @@
+/*
+Given a matrix of characters that contains encoded message. The message for the matrix would be I R O C L E D.
+Travers the matrix in zigzag way, to decode the message.
+*/
+
+const input = [
+    ['I', 'B', 'C', 'A', 'L', 'K', 'A'],
+    ['D', 'R', 'F', 'C', 'A', 'E', 'A'],
+    ['G', 'H', 'O', 'E', 'L', 'A', 'D'],
+];
+
+function decode(input) {
+    const maxRow = input.length - 1; // 1
+    const maxColumn = input[0].length - 1; // 2
+    const res = [];// 1, 5
+
+    let direction = 'bottom';
+    let col = 0 // 2, 0
+    let row = 0;
+
+    while (col <= maxColumn) {
+        const cell = input[row][col];
+        res.push(cell);
+
+        col += 1;
+
+
+        if (direction === 'bottom') {
+            if (row < maxRow) {
+                row += 1;
+            } else {
+                row -= 1;
+                direction = 'top';
+            }
+
+            continue;
+        }
+
+        if (direction === 'top') {
+            if (row > maxRow) {
+                row -= 1;
+            } else {
+                row += 1;
+                direction = 'bottom';
+            }
+        }
+    }
+
+    return res;
+}
+
+console.log(decode(input)); // [I R O C L E D];
