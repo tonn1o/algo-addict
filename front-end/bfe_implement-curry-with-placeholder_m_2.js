49cad05961880e98e3298f4f2ecfcc3e4a09fb68
Toni Babenko <a.babenko.e@gmail.com>
2022-02-07 10:41:44 +0100

    add 2

--- front-end/bfe_implement-curry-with-placeholder_m_2.js
+++ front-end/bfe_implement-curry-with-placeholder_m_2.js
@@ -1,55 +1,33 @@
-
-
-
-
-
-
 /**
  * @param { (...args: any[]) => any } fn
  * @returns { (...args: any[]) => any }
  */
+
 function curry(fn) {
     const placeholder = curry.placeholder;
 
-    return (function inner(oldArgs, ...newArgs) {
-        const combinedArgs = [...oldArgs];
+    return function curried(...args) {
+        const sanitizedArgs = args.splice(0, fn.length);
+        const hasPlaceholders = sanitizedArgs.includes(placeholder);
 
-        let skipPlaceholdersNum = 0;
-        for (let newArg of newArgs.slice(0, fn.length)) {
-            if (newArg === placeholder) {
-                skipPlaceholdersNum += 1;
-            } else {
-                for (let i = 0; i < combinedArgs.length; i++ ) {
-                    if (combinedArgs[i] === placeholder) {
-                        if (skipPlaceholdersNum < 1) {
-                            combinedArgs[i] = newArg;
-                            break;
-                        } else {
-                            skipPlaceholdersNum -= 1;
-                        }
-                    }
+        if (!hasPlaceholders && sanitizedArgs.length >= fn.length) {
+            return fn.apply(this, sanitizedArgs);
+        }
+
+        return function next(...newArgs) {
+            const mergedArgs = [];
+
+            sanitizedArgs.forEach(arg => {
+                if (arg === placeholder && newArgs.length) {
+                    mergedArgs.push(newArgs.shift());
+                } else {
+                    mergedArgs.push(arg);
                 }
-            }
-        }
+            });
 
-        if (combinedArgs.indexOf(placeholder) === -1) {
-            return fn.apply(this, combinedArgs);
-        } else {
-            return inner.bind(this, combinedArgs);
+            return curried.apply(this, [...mergedArgs, ...newArgs]);
         }
-    })(Array(fn.length).fill(placeholder));
+    }
 }
-
-
-// TEST
 
 curry.placeholder = Symbol()
-
-const  join = (a, b, c) => {
-    return `${a}_${b}_${c}`
-}
-
-const curriedJoin = curry(join)
-const _ = curry.placeholder
-
-curry(join)(_,_,3,4)(1,_)(2,5)
